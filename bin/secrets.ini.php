ccec2cbfe96771830f44043b3df966289bcee973
geta6 <getakura@gmail.com>
2012-03-20 22:47:38 +0900

    add secret.ini.php

---
+++ bin/secrets.ini.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+  'db' => array(
+    'driver'    => 'mysql',
+    'dbname'    => '',
+    'host'      => 'localhost',
+    'user'      => '',
+    'password'  => '',
+    'debug'     => false,
+  ),
+
+  'nginx' => array(
+    'hash'     => 'SECRET_PATH'
+  ),
+
+);
